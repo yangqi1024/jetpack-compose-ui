6fa4aa9ab80a6abb039a368d3a5b56dd9be6d3df
yangqi <yangq138@chinaunicom.cn>
2022-04-01 14:25:34 +0800

    feat:新增NoticeBar

---
+++ app/src/main/java/cn/idesign/cui/testclient/noticebar/NoticeBarTest.kt
@@ -0,0 +1,76 @@
+package cn.idesign.cui.testclient.noticebar
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import cn.idesign.cui.noticebar.NoticeBar
+
+@Composable
+fun NoticeBarTest() {
+    LazyColumn(Modifier.padding(10.dp), verticalArrangement = Arrangement.spacedBy(10.dp)) {
+        item {
+            Text(text = "基础用法")
+            NoticeBar(text = "这是 NoticeBar 通告栏")
+        }
+
+        item {
+
+            Text(text = "单行用法")
+            NoticeBar(text = "[单行] 这是NoticeBar通告栏，这是NoticeBar通告栏，这是NoticeBar通告栏", singleLine = true)
+        }
+
+        item {
+
+            Text(text = "多行用法")
+            NoticeBar(text = "[多行] 这是NoticeBar通告栏，这是NoticeBar通告栏，这是NoticeBar通告栏")
+        }
+
+        item {
+
+            Text(text = "前缀图标")
+            NoticeBar(text = "这是 NoticeBar 通告栏", prefixIcon = Icons.Default.Notifications)
+        }
+
+        item {
+
+            Text(text = "关闭图标")
+            NoticeBar(
+                text = "这是 NoticeBar 通告栏",
+                prefixIcon = Icons.Default.Notifications,
+                showClose = true
+            )
+        }
+
+        item {
+
+
+            Text(text = "自定义样式")
+            NoticeBar(
+                text = "这是 NoticeBar 通告栏",
+                prefixIcon = Icons.Default.Notifications,
+                showClose = true,
+                color = MaterialTheme.colors.primary.copy(ContentAlpha.medium)
+            )
+        }
+
+//        item {
+//
+//            Text(text = "文字滚动")
+//            NoticeBar(
+//                text = "这是 NoticeBar 通告栏",
+//                prefixIcon = Icons.Default.Notifications,
+//                showClose = true,
+//                scrollable = true,
+//                color = MaterialTheme.colors.primary.copy(ContentAlpha.medium)
+//            )
+//        }
+    }
+}
---
+++ cui/src/main/java/cn/idesign/cui/noticebar/NoticeBar.kt
@@ -0,0 +1,114 @@
+package cn.idesign.cui.noticebar
+
+import android.util.Log
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NoticeBar(
+    text: String,
+    color: Color = Color(0xFFDE8C17),
+    backgroundColor: Color = Color(0xFFFCF6ED),
+    singleLine: Boolean = false,
+    showClose: Boolean = false,
+    scrollable: Boolean = false,
+    iconSize: Dp = 16.dp,
+    prefixIcon: ImageVector? = null,
+    style: TextStyle = MaterialTheme.typography.body2,
+    overflow: TextOverflow = TextOverflow.Ellipsis
+) {
+
+    var show by remember {
+        mutableStateOf(true)
+    }
+    val rememberInfiniteTransition = rememberInfiniteTransition()
+    var animProgress by remember {
+        mutableStateOf(0f)
+    }
+    if(scrollable){
+        animProgress = rememberInfiniteTransition.animateFloat(
+            initialValue = 0f, targetValue = -1000f, animationSpec = infiniteRepeatable(
+                tween(
+                    5000,
+                    easing = LinearEasing
+                )
+            )
+        ).value
+    }
+    Log.d("NoticeBar","animProgress:${animProgress}")
+
+
+    if (show) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(backgroundColor)
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            prefixIcon?.let {
+                Icon(
+                    it,
+                    contentDescription = null,
+                    tint = color,
+                    modifier = Modifier.size(iconSize)
+                )
+            }
+
+            Text(
+                text = text,
+                color = color,
+                overflow = overflow,
+                maxLines = if (singleLine) 1 else Int.MAX_VALUE,
+                modifier = Modifier
+                    .graphicsLayer{
+                        translationX = animProgress
+                    }
+                    .weight(1f)
+                    .padding(horizontal = 5.dp),
+                style = style
+            )
+            if (showClose) {
+                Icon(
+                    Icons.Default.Close,
+                    contentDescription = null,
+                    tint = color,
+                    modifier = Modifier
+                        .size(iconSize)
+                        .clickable {
+                            show = false
+                        },
+                )
+            }
+        }
+    }
+}
